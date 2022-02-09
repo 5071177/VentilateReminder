baaa7cde35fb178ddb16708d842ca56ba3be7cbb
Yury Prokhorov <yury.prokhorov@appewa.com>
2022-02-09 21:03:21 +0300

    Initial Commit

---
+++ VentilateReminder/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  VentilateReminder
+//
+//  Created by Yury Prokhorov on 09.02.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ VentilateReminder/VentilateReminderApp.swift
@@ -0,0 +1,17 @@
+//
+//  VentilateReminderApp.swift
+//  VentilateReminder
+//
+//  Created by Yury Prokhorov on 09.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct VentilateReminderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
